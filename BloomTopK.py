d54a0a7acd21010251c7a0844bd15615c0f57388
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-04-30 22:04:49 -0300

    Time series revision

--- BloomTopK.py
+++ BloomTopK.py
@@ -3,7 +3,7 @@ import csv
 import datetime
 from redis import RedisError
 
-r = redis.Redis(password='YOUR_PASSWORD', host='YOUR_ENDPOINT_HOST', port=YOUR_ENDPOINT_PORT, decode_responses=True)
+r = redis.Redis(password='O3lnmWPfc8dG4SmToXPYnV0q3BsbD8eG', host='redis-19787.c16.us-east-1-3.ec2.cloud.redislabs.com', port=19787, decode_responses=True)
 
 r.flushall()
 
--- SubOrders.py
+++ SubOrders.py
@@ -2,7 +2,7 @@ import redis
 import datetime
 import time
 
-r = redis.Redis(password='O3lnmWPfc8dG4SmToXPYnV0q3BsbD8eG', host='redis-19787.c16.us-east-1-3.ec2.cloud.redislabs.com', port=19787)
+r = redis.Redis(password='O3lnmWPfc8dG4SmToXPYnV0q3BsbD8eG', host='redis-19787.c16.us-east-1-3.ec2.cloud.redislabs.com', port=19787, decode_responses=True)
 
 while True:
 	received = r.xread({"orders": '$'}, None, 0)
@@ -15,5 +15,5 @@ while True:
 			orderDict = tuple[1];
 			print(orderDict)
 
-#	time.sleep(1)
+	time.sleep(1)
 
